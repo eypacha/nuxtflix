b08ac0d45a3fd9f3b6cd568ce634649b6ecca333
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-20 18:26:52 -0300

    feat: update nuxt.config.ts with Vuetify theme settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["vuetify-nuxt-module"]
+  modules: ["vuetify-nuxt-module"],
+  vuetify: {
+    moduleOptions: {
+      // styles: {
+      //   configFile: 'assets/styles/settings.scss'
+      // }
+    },
+    vuetifyOptions: {
+      theme: {
+        defaultTheme: 'dark',
+        themes: {
+          dark: {
+            colors: {
+              primary: '#00dc82',
+              background: '#020420',
+            }
+          }
+        }
+      }
+    }
+  },
 })
