50f3fa8f87bccbac19f5d7a19b41d29e6ea54784
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-22 18:19:09 -0300

    style: refactor header component

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,11 +1,44 @@
 <template>
-    <v-app-bar app elevation="0" class="pr-4" color="transparent">
+    <v-app-bar app elevation="0" class="pr-4" :color="bg">
         <v-toolbar-title>
             <router-link to="/">
                 <v-img src="/img/logo.svg" width="100" contain alt="Nutflix" />
             </router-link>
         </v-toolbar-title>
         <v-spacer />
-        <v-btn color="primary" variant="flat" to="/login">Iniciar Sesión</v-btn>
+        <v-btn v-if="isIndexPage" color="primary" variant="flat" to="/login">Iniciar Sesión</v-btn>
     </v-app-bar>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { useRoute } from 'nuxt/app'
+
+const route = useRoute()
+
+const isIndexPage = computed((): boolean => route.path === '/');
+
+const bg = ref<string>('transparent');
+
+const changeColor = ():void => {
+  if (window.scrollY > 100) {
+    bg.value = '#020420';
+  } else {
+    bg.value = 'transparent';
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', changeColor);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', changeColor);
+});
+</script>
+
+<style scoped>
+header {
+    transition: background-color 400ms;
+}
+</style>
