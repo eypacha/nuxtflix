074d20b9d47964b5693a302429294b94a57d49db
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-22 19:22:20 -0300

    chore: install sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['@/assets/scss/main.scss'],
   modules: ["vuetify-nuxt-module", "@nuxt/eslint"],
   vuetify: {
     moduleOptions: {
