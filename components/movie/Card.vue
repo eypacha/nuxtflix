0a51fe8f563e3de2d67eeb3ea950b87be82748bb
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-24 17:11:24 -0300

    feat: movie placeholder asset for undefined posters

--- components/movie/Card.vue
+++ components/movie/Card.vue
@@ -1,15 +1,15 @@
 <template>
-    <v-hover v-slot:default="{ isHovering, props }">
+    <v-hover v-slot="{ isHovering, props }">
         <v-card
             v-bind="props"
             :border="`sm opacity-100 ${isHovering ? 'primary' : 'background'}`"
             rounded="lg" 
-            color="grey-lighten-1"
+            color="background"
             height="200" 
             width="130"
             class="movie mx-2">
             <v-img
-            :src="movie.poster"
+            :src="movie.poster || '/img/movie-placeholder.png'"
             class="align-end"
             height="200px"
             width="130px"
@@ -18,8 +18,7 @@
                 <v-expand-transition>
                     <v-card-text
                         v-if="isHovering"
-                        class="bg-background opacity-80 text-center text-white px-1 py-2"
-                        v-text="movie.title" />
+                        class="bg-background opacity-80 text-center text-white px-1 py-2">{{ movie.title }}</v-card-text>
                 </v-expand-transition>
             </v-img>
         </v-card>
