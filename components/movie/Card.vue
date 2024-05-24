c165377ab0a80a2770ea3472c6435d54da485fd1
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-24 15:59:30 -0300

    feat: create Card component for displaying movie poster and title

---
+++ components/movie/Card.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-hover v-slot:default="{ isHovering, props }">
+        <v-card
+            v-bind="props"
+            :border="`sm opacity-100 ${isHovering ? 'primary' : 'background'}`"
+            rounded="lg" 
+            color="grey-lighten-1"
+            height="200" 
+            width="130"
+            class="movie mx-2">
+            <v-img
+            :src="movie.poster"
+            class="align-end"
+            height="200px"
+            width="130px"
+            cover
+            >
+                <v-expand-transition>
+                    <v-card-text
+                        v-if="isHovering"
+                        class="bg-background opacity-80 text-center text-white px-1 py-2"
+                        v-text="movie.title" />
+                </v-expand-transition>
+            </v-img>
+        </v-card>
+    </v-hover>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface Movie {
+  title: string;
+  imdbID: string;
+  poster?: string;
+}
+
+const props = defineProps<{
+  movie: Movie
+}>();
+</script>
+
+<style scoped lang="scss">
+.v-card-text {
+    line-height: 1.2;
+    text-wrap: balance;
+}
+</style>
