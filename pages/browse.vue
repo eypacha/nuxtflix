77e30534b495c46895eb58996f3036de94f2f536
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-24 18:11:36 -0300

    feat: create browse page with test content

--- pages/browse.vue
+++ pages/browse.vue
@@ -1,5 +1,33 @@
 <template>
-    <v-container class="fill-height">
-        MOVIE LIST
+    <v-container class="pa-0" :fluid="true">
+        <ParallaxSection class="mb-10" height="calc(100dvh - 300px)" />
+        
+        <MovieSection v-for="movie in movies" :searchTerm="movie.searchTerm" :title="movie.title"/>
     </v-container>
 </template>
+
+<script setup lang="ts">
+
+const movies = [
+    {
+        searchTerm: 'avengers',
+        title: 'The Avengers'
+    },
+    {
+        searchTerm: 'batman',
+        title: 'Batmania'
+    },
+    {
+        searchTerm: 'lego',
+        title: 'Made with brik'
+    },
+    {
+        searchTerm: 'star wars',
+        title: 'In a galaxy far, far away...'
+    },
+    {
+        searchTerm: 'pink panther',
+        title: 'Pink'
+    },
+]
+</script>
