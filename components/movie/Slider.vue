dcdd18cc0c041f0c671c1e310e8d3eb2aa60cf9d
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-24 17:13:28 -0300

    refactor: title prop moved to section component

--- components/movie/Slider.vue
+++ components/movie/Slider.vue
@@ -1,15 +1,11 @@
 <template>
-    <v-sheet class="mx-auto mt-2 mb-9" elevation="0" color="transparent" max-width="100vw">
-        
-        <h3 class="ml-16">{{ props.title }}</h3>
-        <v-slide-group v-model="model" class="movie-slider pt-2 pb-4" selected-class="bg-success" show-arrows>
-            <v-slide-group-item v-for="movie in props.movies" :key="movie.imdbID">
-                <nuxt-link :to="`watch/${movie.imdbID}`" class="text-decoration-none">
-                    <MovieCard :movie="movie" />
-                </nuxt-link>
-            </v-slide-group-item>
-        </v-slide-group>
-    </v-sheet>
+    <v-slide-group v-model="model" class="movie-slider pt-2 pb-4" selected-class="bg-success" show-arrows>
+        <v-slide-group-item v-for="movie in props.movies" :key="movie.imdbID">
+            <nuxt-link :to="`watch/${movie.imdbID}`" class="text-decoration-none">
+                <MovieCard :movie="movie" />
+            </nuxt-link>
+        </v-slide-group-item>
+    </v-slide-group>
 </template>
 
 <script setup lang="ts">
@@ -25,7 +21,6 @@ interface MovieSearchResult {
 }
 
 interface Props {
-    title?: string,
     movies: MovieSearchResult[]
 }
 
