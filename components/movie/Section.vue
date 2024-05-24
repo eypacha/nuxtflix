25da7fe5d9731b8076ec164a678a12f4ad79004c
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-24 17:12:46 -0300

    feat: create Section component for parenting movie slider

---
+++ components/movie/Section.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-sheet class="mx-auto mt-2 mb-9" elevation="0" color="transparent" max-width="100vw">
+        <h3 class="ml-16">{{ props.title }}</h3>
+        <MovieSlider :movies="searchResults"/>
+    </v-sheet>
+</template>
+
+<script setup lang="ts">
+
+const nuxtApp = useNuxtApp();
+const config = useRuntimeConfig();
+
+interface Props {
+    title: string,
+    searchTerm: string
+}
+
+const props = defineProps<Props>();
+
+interface SearchResult {
+    Title: string;
+    imdbID: string;
+    Poster: string;
+}
+
+interface SearchResponse {
+    Response: string;
+    Search?: SearchResult[];
+}
+
+interface MovieSearchResult {
+    title: string;
+    imdbID: string;
+    poster?: string;
+}
+
+function sanitizeData(value: string): string | undefined {
+    return value === 'N/A' ? undefined : value;
+}
+
+const { data: searchResults } = await useFetch('https://omdbapi.com', {
+    query: {
+        apikey: config.public.omdbApiKey,
+        s: props.searchTerm,
+        type: 'movie'
+    },
+    lazy: false,
+    getCachedData: (key): SearchResponse => {
+        return nuxtApp.payload.data[key] || nuxtApp.static.data[key]
+    },
+    transform: (data: SearchResponse): MovieSearchResult[] => {
+        if (data.Response === 'True' && data.Search) {
+            return data.Search.map(result => ({
+                title: result.Title,
+                imdbID: result.imdbID,
+                poster: sanitizeData(result.Poster),
+            }));
+        }
+        return [];
+    },
+});
+</script>
