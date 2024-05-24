786418a48208a3c25f6524cfff6c1096dd629c0d
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-24 15:35:20 -0300

    fix: correct image path in v-parallax component

--- components/HeroPosters.vue
+++ components/HeroPosters.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-parallax src="img/background.jpg" height="100dvh">
+    <v-parallax src="/img/background.jpg" height="100dvh">
     <div class="d-flex flex-column fill-height justify-center align-center text-white">
      <slot />
     </div>
