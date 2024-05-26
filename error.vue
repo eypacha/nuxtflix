365b48a9510931e2a0ae9645a3b6c3ff2071bcfb
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-26 14:41:25 -0300

    styles: canvas size to 100% in error page

--- error.vue
+++ error.vue
@@ -103,6 +103,8 @@ canvas {
   position: absolute;
   top: 0;
   left: 0;
+  width: 100%;
+  height: 100%;
   -webkit-transform: translate3d(0, 0, 0);
   -moz-transform: translate3d(0, 0, 0);
   transform: translate3d(0, 0, 0);
