e95ad8a528a4bad5de5dbfac6900affd2df40863
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-26 18:19:13 -0300

    styles: padding and center items in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-   <ParallaxSection height="100dvh">
-        <h1 class="text-h3 mb-4">{{ $t('index.title') }}</h1>
-        <h2 class="text-h5">{{ $t('index.subtitle') }}</h2>
+   <ParallaxSection height="100dvh" class="pa-5">
+        <h1 class="text-h3 text-center mb-4">{{ $t('index.title') }}</h1>
+        <h2 class="text-h5 text-center">{{ $t('index.subtitle') }}</h2>
     </ParallaxSection>
 </template>
