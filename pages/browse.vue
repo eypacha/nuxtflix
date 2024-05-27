e73e879e4ba93edd09082d9f2fda2724910aa0b6
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-26 22:59:21 -0300

    styles: delete container in browse page

--- pages/browse.vue
+++ pages/browse.vue
@@ -1,9 +1,6 @@
 <template>
-    <v-container class="pa-0" :fluid="true">
-        <ParallaxSection class="mb-10" height="calc(100dvh - 300px)" />
-        
-        <MovieSection v-for="(movie, index) in movies" :key="index" :search-term="movie.searchTerm" :title="movie.title"/>
-    </v-container>
+    <ParallaxSection class="mb-10" height="calc(100dvh - 300px)" />
+    <MovieSection v-for="(movie, index) in movies" :key="index" :search-term="movie.searchTerm" :title="movie.title"/>
 </template>
 
 <script setup lang="ts">
