1a46e9d1538c0b3000d5ca4c46db75dfab23bd68
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-27 19:48:09 -0300

    refactor: enhance movie component layout and styling

--- pages/watch/[id].vue
+++ pages/watch/[id].vue
@@ -1,38 +1,68 @@
 <template>
-    <v-container class="fill-height">
-        <div v-if="pending">
-            Loading..
-        </div>
+    <v-container class="hero" fluid>
+        <div class="fill-height d-flex align-center justify-center">
 
-        <v-row v-else-if="movie.response">
-            <v-col v-if="movie.poster" cols="4" class="d-flex justify-end">
-                <v-img :src="movie.poster" :alt="movie.title" max-width="266"/>
+        <v-row v-if="movie && movie.response" justify="center" class="mt-15 mb-10 mt-sm-0 mt-sm-0">
+            <v-col cols="12" sm="5" order-sm="2">
+                <v-card outlined color="background" class="fill-height d-flex flex-column pa-7" rounded="lg">
+                    <v-card-title class="text-h4">{{ movie.title }}</v-card-title>
+                    <v-card-subtitle v-if="movie.rated || movie.runtime || movie.genre" class="mb-4">
+                        <div class="text-primary mb-2 gap-dots">
+                            <span v-if="movie.rated">{{ movie.rated }}</span>
+                            <span v-if="movie.runtime">{{ movie.runtime }}</span>
+                            <span v-if="movie.genre">{{ movie.genre }}</span>
+                        </div>
+                        <div>
+                            <span v-if="movie.imdbRating" class="mr-3">
+                                <v-icon icon="mdi-star" size="small" color="yellow-darken-2"/> {{ movie.imdbRating }}
+                            </span>
+                            <span v-if="movie.awards">
+                              <v-icon icon="mdi-medal" size="small" color="yellow-darken-2"/> {{ movie.awards }}
+                            </span>
+                        </div>
+                    </v-card-subtitle>
+                    <v-card-text class="text-body-1">
+                        <p class="plot mb-4">{{ movie.plot }}</p>
+                        <p v-if="movie.director" class="mb-1">
+                            <span class="text-primary mr-2">{{ $t('watch.director') }}</span> {{ movie.director }}
+                        </p>
+                        <p v-if="movie.writer" class="mb-1">
+                            <span class="text-primary mr-2">{{ $t('watch.writer') }}</span> {{ movie.writer }}
+                        </p>
+                        <p v-if="movie.actors" class="mb-1">
+                            <span class="text-primary mr-2">{{ $t('watch.cast') }}</span> {{ movie.actors }}
+                        </p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn
+                            variant="flat"
+                            small
+                            icon
+                            color="grey-darken-2"
+                            to="/browse"
+                            :aria-label="$t('watch.go_back')">
+                            <v-icon color="background" icon="mdi-chevron-left"/>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-col>
-            <v-col cols="8">
-                <h3 class="text-h3">{{ movie.title }}</h3>
-                <div class="text-primary">
-                    {{ movie.runtime }} {{ movie.genre }} 
-                </div>
-                <v-rating
-                    v-if="movie.imdbRating"
-                    :model-value="movie.imdbRating"
-                    half-increments
-                    readonly
-                    length="10"
-                    size="24"
-                    color="grey-lighten-1"
-                    active-color="yellow-darken-2"
-                    /> 
-                <p> {{ movie.plot }}</p>
+            <v-col v-if="movie.poster" cols="12" sm="2" class="d-flex justify-center justify-sm-end" order-sm="1">
+                <v-img :src="movie.poster" :alt="movie.title" class="movie-poster" max-width="320" rounded="lg"/>
             </v-col>
         </v-row>
 
         <div v-else>
-            <h3 class="text-h3 mb-4">No encontramos la película</h3>
-            <h4 class="text-h4">Encontrarás otros títulos al <NuxtLink to="/browse">inicio</NuxtLink>
+            <h3 class="text-h3 mb-4">
+                {{ $t('watch.no_movie') }}
+            </h3>
+            <h4 class="text-h4">
+                {{ $t('watch.find_other_titles') }} <NuxtLink to="/browse">{{ $t('watch.home') }} </NuxtLink>
             </h4>
         </div>
+
+    </div>
     </v-container>
+    
 </template>
 
 <script setup lang="ts">
@@ -52,8 +82,12 @@ definePageMeta({
 interface MovieData {
     Response: string;
     Title?: string;
+    Rated?: string;
     Runtime?: string;
     Genre?: string;
+    Director?: string;
+    Writer?: string;
+    Actors?: string;
     Plot?: string;
     imdbRating?: string;
     Awards?: string;
@@ -63,48 +97,102 @@ interface MovieData {
 interface Movie {
     response: boolean;
     title?: string;
+    rated?: string;
     runtime?: string;
     genre?: string;
+    director?: string;
+    writer?: string;
+    actors?: string;
     plot?: string;
     imdbRating?: number;
-    Awards?: string;
+    imdbStars?: number;
+    awards?: string;
     poster?: string;
 };
 
-function sanitizeData(value: string): string | undefined {
+function sanitizeString(value: string): string | undefined {
     return value === 'N/A' ? undefined : value;
 };
 
-function sanitizeRating(value: string): number | undefined {
-    if (value === 'N/A') return undefined;
-    // To round to the nearest 0.5
-    return Math.round(parseFloat(value) * 2) / 2; 
+function sanitizeNumber(value: string): number | undefined {
+    return value === 'N/A' ? undefined : parseFloat(value);
 };
 
 const { pending, data: movie } = await useFetch('https://omdbapi.com', {
     query: {
         apikey: config.public.omdbApiKey,
-        i: route.params.id,
+        i: route.params.id
     },
     lazy: false,
-    pick: ['response', 'title', 'runtime', 'genre', 'plot', 'poster','imdbRating'],
+    pick: ['response', 'title', 'runtime', 'rated', 'genre', 'director', 'writer', 'actors', 'plot', 'poster', 'awards', 'imdbRating', 'imdbStars'],
     getCachedData: (key): Movie => {
         return nuxtApp.payload.data[key] || nuxtApp.static.data[key]
     },
     transform: (data: MovieData): Movie => {
 
         if(data.Response === 'False') return undefined
-        
-        console.log("data",data)
+
+        const imdbRating = sanitizeNumber(data.imdbRating);
+
+        const imdbStars = imdbRating !== undefined ? Math.round(imdbRating) / 2 : undefined;
+
         return {
             response: data.Response === 'True',
             title: data.Title,
-            runtime: sanitizeData(data.Runtime),
-            imdbRating: sanitizeRating(data.imdbRating),
-            genre: sanitizeData(data.Genre),
-            plot: sanitizeData(data.Plot),
-            poster: sanitizeData(data.Poster)
+            runtime: sanitizeString(data.Runtime),
+            rated: sanitizeString(data.Rated),
+            genre: sanitizeString(data.Genre),
+            director: sanitizeString(data.Director),
+            writer: sanitizeString(data.Writer),
+            actors: sanitizeString(data.Actors),
+            plot: sanitizeString(data.Plot),
+            poster: sanitizeString(data.Poster),
+            awards: sanitizeString(data.Awards),
+            imdbRating,
+            imdbStars,
         };
     },
 });
+
 </script>
+
+<style scoped lang="scss">
+.hero {
+    min-height: 100dvh;
+    background-image: url(/img/background.jpg);
+    background-repeat: repeat;
+}
+
+.v-card-title {
+    line-height: 1.2 !important;
+    white-space: normal !important;
+}
+
+.v-card-subtitle {
+
+    .gap-dots {
+        gap: .5em;
+        white-space: normal !important;
+
+         span:not(:last-child):after {
+            content: '•';
+            margin-inline: .5em;
+        }
+    }
+    
+}
+
+</style>
+
+<style lang="scss">
+.movie-poster {
+    img {
+        object-position: top;
+    }
+}
+
+.v-list-item__spacer {
+    width: 16px !important;
+}
+
+</style>
