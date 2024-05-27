84bc5b428fa6aa375fbc6efef98a40990a94aa4b
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-26 22:22:21 -0300

    styles: always show icons in movie-slider

--- components/movie/Slider.vue
+++ components/movie/Slider.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-slide-group v-model="model" class="movie-slider pt-2 pb-4" selected-class="bg-success" show-arrows>
+    <v-slide-group v-model="model" class="movie-slider pt-2 pb-4" selected-class="bg-success" show-arrows="always">
         <v-slide-group-item v-for="movie in props.movies" :key="movie.imdbID">
             <nuxt-link :to="`watch/${movie.imdbID}`" class="text-decoration-none">
                 <MovieCard :movie="movie" />
