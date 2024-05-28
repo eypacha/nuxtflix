033c4c0f923237d243853dffff002a47be4fdd20
Ey Pacha! <pachaguionbajo@gmail.com>
2024-05-28 10:52:40 -0300

    test: add index_route class for easy testing

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-   <ParallaxSection height="100dvh" class="pa-5">
+   <ParallaxSection height="100dvh" class="index_route pa-5">
         <h1 class="text-h3 text-center mb-4">{{ $t('index.title') }}</h1>
         <h2 class="text-h5 text-center">{{ $t('index.subtitle') }}</h2>
     </ParallaxSection>
